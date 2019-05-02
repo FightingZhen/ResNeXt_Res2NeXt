62fe3307c4f22713710c6714d308f3f070417c2f
FightingZhen <295632982@qq.com>
2019-05-02 18:59:56 +0800

    Add files via upload

--- Res2NeXt/res2next_model.py
+++ Res2NeXt/res2next_model.py
@@ -9,9 +9,9 @@ import time
 
 
 class Res2NeXt(object):
-    def __init__(self, model_name, sess, train_data, tst_data, epoch, num_class, ksize, weight_decay, cardinality,
-                 width, scale, block_num1, block_num2, block_num3, learning_rate, batch_size, img_height, img_width,
-                 use_se):
+    def __init__(self, model_name, sess, train_data, tst_data, epoch, num_class, ksize, weight_decay, momentum,
+                 cardinality, width, scale, block_num1, block_num2, block_num3, learning_rate, batch_size, img_height,
+                 img_width, use_se):
 
         self.sess = sess
         self.training_data = train_data
@@ -21,6 +21,7 @@ class Res2NeXt(object):
         self.ckptDir = '../checkpoint/' + self.model + '/'
         self.k = ksize
         self.wd = weight_decay
+        self.momentum = momentum
         self.c = cardinality
         self.w = width
         self.s = scale
@@ -47,6 +48,7 @@ class Res2NeXt(object):
         save2file('epoch : %d' % self.eps, self.ckptDir, self.model)
         save2file('ksize : %d' % self.k, self.ckptDir, self.model)
         save2file('weight decay : %g' % self.wd, self.ckptDir, self.model)
+        save2file('momentum : %g' % self.momentum, self.ckptDir, self.model)
         save2file('cardinality : %d' % self.c, self.ckptDir, self.model)
         save2file('width : %d' % self.w, self.ckptDir, self.model)
         save2file('scale : %d' % self.s, self.ckptDir, self.model)
@@ -120,9 +122,9 @@ class Res2NeXt(object):
 
             return scaled
 
-    def first_layer(self, inputMap, is_training, scope_name):
+    def first_layer(self, inputMap, out_channel, is_training, scope_name):
         with tf.variable_scope(scope_name):
-            _conv = self.conv(inputMap, out_channel=self.oc1, ksize=self.k, stride=1, scope_name='_conv')
+            _conv = self.conv(inputMap, out_channel=out_channel, ksize=self.k, stride=1, scope_name='_conv')
             _bn = self.bn(_conv, scope_name='_bn', is_training=is_training)
             _relu = self.relu(_bn, scope_name='_relu')
             return _relu
@@ -222,7 +224,8 @@ class Res2NeXt(object):
     def resnext_model(self, inputMap, model_name, ksize, scale, block_num1, block_num2, block_num3, out_channel1,
                       out_channel2, out_channel3, is_training, reuse):
         with tf.variable_scope(model_name, reuse=reuse):
-            _first_layer = self.first_layer(inputMap, is_training=is_training, scope_name='_first_layer')
+            _first_layer = self.first_layer(inputMap, out_channel=64, is_training=is_training,
+                                            scope_name='_first_layer')
 
             stage1 = self.residual_stage(inputMap=_first_layer,
                                          ksize=ksize,
@@ -290,7 +293,7 @@ class Res2NeXt(object):
         with tf.variable_scope('optimize'):
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies(update_ops):
-                self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+                self.train_op = tf.train.MomentumOptimizer(self.lr, momentum=self.momentum).minimize(self.loss)
 
         with tf.variable_scope('tfSummary'):
             self.merged = tf.summary.merge_all()
@@ -312,50 +315,47 @@ class Res2NeXt(object):
     def train(self):
         self.sess.run(tf.global_variables_initializer())
         self.itr_epoch = len(self.training_data[0]) // self.bs
-        self.total_iteration = self.eps * self.itr_epoch
 
         training_acc = 0.0
         training_loss = 0.0
 
-        for itr in range(1, self.total_iteration + 1):
-            if itr == int(self.total_iteration * 0.3) or itr == int(self.total_iteration * 0.6):
+        for e in range(1, self.eps + 1):
+            if e == 150 or e == 225:
                 self.lr *= 0.1
+            for itr in range(self.itr_epoch):
+                _index = np.random.randint(low=0, high=len(self.training_data[0]), size=self.bs)
+                _tr_img_batch = self.training_data[0][_index]
+                _tr_lab_batch = self.training_data[1][_index]
 
-            _index = np.random.randint(low=0, high=len(self.training_data[0]), size=self.bs)
-            _tr_img_batch = self.training_data[0][_index]
-            _tr_lab_batch = self.training_data[1][_index]
+                _tr_img_batch = data_augmentation(_tr_img_batch)
 
-            _tr_img_batch = data_augmentation(_tr_img_batch)
+                _train_accuracy, _train_loss, _ = self.sess.run([self.accuracy, self.cost, self.train_op],
+                                                                feed_dict={self.x: _tr_img_batch,
+                                                                           self.y: _tr_lab_batch,
+                                                                           self.is_training: True})
+                training_acc += _train_accuracy
+                training_loss += _train_loss
 
-            _train_accuracy, _train_loss, _ = self.sess.run([self.accuracy, self.cost, self.train_op],
-                                                            feed_dict={self.x: _tr_img_batch,
-                                                                       self.y: _tr_lab_batch,
-                                                                       self.is_training: True})
-            training_acc += _train_accuracy
-            training_loss += _train_loss
+            summary = self.sess.run(self.merged, feed_dict={self.x: _tr_img_batch,
+                                                            self.y: _tr_lab_batch,
+                                                            self.is_training: False})
 
-            if itr % self.itr_epoch == 0:
-                _current_eps = int(itr / self.itr_epoch)
-                summary = self.sess.run(self.merged, feed_dict={self.x: _tr_img_batch,
-                                                                self.y: _tr_lab_batch,
-                                                                self.is_training: False})
+            training_acc = float(training_acc / self.itr_epoch)
+            training_loss = float(training_loss / self.itr_epoch)
 
-                training_acc = float(training_acc / self.itr_epoch)
-                training_loss = float(training_loss / self.itr_epoch)
+            log = "Epoch: [%d], Training Accuracy: [%g], Training Loss: [%g], Learning Rate: [%f], Time: [%s]" % \
+                  (e, training_acc, training_loss, self.lr, time.ctime(time.time()))
 
-                log = "Epoch: [%d], Training Accuracy: [%g], Training Loss: [%g], Learning Rate: [%f], Time: [%s]" % \
-                      (_current_eps, training_acc, training_loss, self.lr, time.ctime(time.time()))
+            save2file(log, self.ckptDir, self.model)
 
-                save2file(log, self.ckptDir, self.model)
+            self.writer.add_summary(summary, e)
 
-                self.writer.add_summary(summary, _current_eps)
+            self.saver.save(self.sess, self.ckptDir + self.model + '-' + str(e))
 
-                self.saver.save(self.sess, self.ckptDir + self.model + '-' + str(_current_eps))
+            eval.test_procedure(test_data=self.test_data, distribution_op=self.distribution, inputX=self.x,
+                                inputY=self.y, mode='test', num_class=self.num_class, batch_size=self.bs,
+                                session=self.sess, is_training=self.is_training, ckptDir=self.ckptDir,
+                                model=self.model)
 
-                eval.test_procedure(test_data=self.test_data, distribution_op=self.distribution, inputX=self.x,
-                                    inputY=self.y, mode='test', num_class=self.num_class, batch_size=self.bs,
-                                    session=self.sess, is_training=self.is_training, ckptDir=self.ckptDir,
-                                    model=self.model)
-
-                training_acc = 0.0
-                training_loss = 0.0
+            training_acc = 0.0
+            training_loss = 0.0
